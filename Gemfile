c3b3d532f55a7540208b8bd839a69da286f96c6f
Claudinei Perboni <cperbony@gmail.com>
2019-03-02 18:20:31 -0300

    Retornando com templates startbootstrap-sb-admin-2 e gentelella

--- Gemfile
+++ Gemfile
@@ -11,15 +11,16 @@ gem 'sqlite3'
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
-# Use Uglifier as compressor for JavaScript assets
+# Use Uglifier as compressor for JavaScript stylesheets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 
-# Use CoffeeScript for .coffee assets and views
+# Use CoffeeScript for .coffee stylesheets and views
 gem 'coffee-rails', '~> 4.2'
+gem 'coffee-script-source', '~> 1.4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
+# gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,37 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
+# Version of your stylesheets, change this if you want to expire all your stylesheets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+# Add additional stylesheets to the asset load path.
+# Rails.application.config.stylesheets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
+# Precompile additional stylesheets.
+# application.js, application.css, and all non-JS/CSS in the app/stylesheets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# /app/assets
+Rails.application.config.assets.precompile += %w[
+  admins_backoffice.js
+  admins_backoffice.css
+  users_backoffice.js
+  users_backoffice.css
+]
+
+# /lib/assets
+Rails.application.config.assets.precompile += %w[
+  sb-admin-2.js
+  sb-admin-2.css
+  custom.js
+  custom.css
+  img.jpg
+]
+
+# /vendor/assets
+Rails.application.config.assets.precompile += %w[
+  jquery-2-2-3/dist/jquery.js
+]
